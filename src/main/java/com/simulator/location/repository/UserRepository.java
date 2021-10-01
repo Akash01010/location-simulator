2d2a956542ac2bd23a1e72b84464d2cf6530d88b
Akash Agrawal <akashagrawal01010@gmail.com>
2021-10-01 13:56:32 +0530

    fix: typo fix

--- src/main/java/com/simulator/location/repository/UserRepository.java
+++ src/main/java/com/simulator/location/repository/UserRepository.java
@@ -5,12 +5,10 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.LinkedList;
 import java.util.List;
 
 
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
-    LinkedList
     List<User> findByName(@Param("name") String name);
 }
